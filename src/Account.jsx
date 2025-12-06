6abc1f02357c20f183eb7c2b1b37ddab91f95655
Antonin Maubert <antoninmaubert224@gmail.com>
2025-12-06 11:11:30 +0100

    transfer

---
+++ src/Account.jsx
@@ -0,0 +1,12 @@
+import './App.jsx'
+// import { useNavigate } from "react-router-dom";
+
+export function Account() {
+    // const navigate = useNavigate();
+
+    return (
+        <div>
+            <h1> Account </h1>
+        </div>
+    )
+}
